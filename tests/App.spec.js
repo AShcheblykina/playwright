4bc25a0c3a368c4b11b59a330c319993584a5317
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-06-21 22:15:26 +0300

    added test2

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -1,17 +1,30 @@
 const { test, expect } = require("@playwright/test");
-const {email_1, password_1} = require("./user.js");
+const {email_1, password_1, email_2} = require("./user.js");
 
 
 test('SuccesAuth', async ({ page }) => {
   await page.goto('https://netology.ru/');
   await page.getByRole('link', { name: 'Войти' }).click();
-  await page.screenshot({ path: 'screenshot.png' });
   await page.getByPlaceholder('Email').click();
   await page.getByPlaceholder('Email').fill(email_1);
   await page.getByPlaceholder('Пароль').click();
   await page.getByPlaceholder('Пароль').fill(password_1);
   await page.getByTestId('login-submit-btn').click();
   await page.getByText('АЩ').click();
+  
+});
+test('NotSuccesAuth', async ({ page }) => {
+  await page.goto('https://netology.ru/');
+  await page.getByRole('link', { name: 'Войти' }).click();
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill(email_2);
+  await page.getByPlaceholder('Пароль').click();
+  await page.getByPlaceholder('Пароль').fill(password_1);
+  await page.getByTestId('login-submit-btn').click();
+  await page.getByTestId('login-error-hint').click({
+    button: 'right'
+  });
+  
 });
 
 
