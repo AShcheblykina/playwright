2ebfce82c0c509e736df4b61015a8eabf2e4aa80
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-06-21 22:46:58 +0300

    added test3

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -4,13 +4,21 @@ const {email_1, password_1, email_2} = require("./user.js");
 
 test('SuccesAuth', async ({ page }) => {
   await page.goto('https://netology.ru/');
+  await page.screenshot({ path:'./screenshots/screenshot1.png' });
   await page.getByRole('link', { name: 'Войти' }).click();
+  await page.screenshot({ path:'./screenshots/screenshot2.png' });
   await page.getByPlaceholder('Email').click();
+  await page.screenshot({ path:'./screenshots/screenshot3.png' });
   await page.getByPlaceholder('Email').fill(email_1);
+  await page.screenshot({ path:'./screenshots/screenshot4.png' });
   await page.getByPlaceholder('Пароль').click();
+  await page.screenshot({ path:'./screenshots/screenshot5.png' });
   await page.getByPlaceholder('Пароль').fill(password_1);
+  await page.screenshot({ path:'./screenshots/screenshot6.png' });
   await page.getByTestId('login-submit-btn').click();
+  await page.screenshot({ path:'./screenshots/screenshot7.png' });
   await page.getByText('АЩ').click();
+  await page.screenshot({ path:'./screenshots/screenshot8.png' });
   
 });
 test('NotSuccesAuth', async ({ page }) => {
