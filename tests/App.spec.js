06b5089bab29772574f0431aa1e57eb1335c3500
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-06-21 21:50:30 +0300

    added test

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -1,31 +1,20 @@
 const { test, expect } = require("@playwright/test");
-const TODO_ITEMS = [
-    user.js
-  ];
-  test('test', async ({ page }) => {
-    // Go to https://netology.ru/?modal=sign_in
-    await page.goto('https://netology.ru/?modal=sign_in');
-    // Click [placeholder="Email"]
-    await page.click('[placeholder="Email"]');
-    // Click [placeholder="Email"]
-    await page.click('[placeholder="Email"]');
-    // Fill [placeholder="Email"]
-    await page.fill('[placeholder="Email"]', email_1);
-    // Click [placeholder="Пароль"]
-    await page.click('[placeholder="Пароль"]');
-    // Click [placeholder="Email"]
-    await page.click('[placeholder="Email"]');
-    // Click [placeholder="Пароль"]
-    await page.click('[placeholder="Пароль"]');
-    // Click [placeholder="Пароль"]
-    await page.click('[placeholder="Пароль"]');
-    // Fill [placeholder="Пароль"]
-    await page.fill('[placeholder="Пароль"]', password_1);
-    // Click [data-testid="login-submit-btn"]
-    await Promise.all([
-      page.waitForNavigation(/*{ url:  }*/),
-      page.click('[data-testid="login-submit-btn"]')
-    ]);
-    // Click text=АЩ
-    await page.click('text=АЩ');
+const {email_1, password_1} = require("./user.js");
+
+
+test('SuccesAuth', async ({ page }) => {
+  await page.goto('https://netology.ru/');
+  await page.getByRole('link', { name: 'Войти' }).click();
+  await page.screenshot({ path: 'screenshot.png' });
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill(email_1);
+  await page.getByPlaceholder('Пароль').click();
+  await page.getByPlaceholder('Пароль').fill(password_1);
+  await page.getByTestId('login-submit-btn').click();
+  await page.getByText('АЩ').click();
 });
+
+
+
+
+
